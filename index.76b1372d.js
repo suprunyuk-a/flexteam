7f1c3bfd01dae98af6b43c9f901520a22bc335c2
suprunyuk-a <103211775+suprunyuk-a@users.noreply.github.com>
2022-06-27 17:55:25 +0000

    Deploying to gh-pages from @ suprunyuk-a/flexteam@707419210af29dfd284c62ead728f1f117b799b1 🚀

---
+++ index.76b1372d.js
@@ -0,0 +1,2 @@
+function myFunction(){document.getElementById("myDropdown").classList.toggle("show")}window.onclick=function(t){if(!t.target.matches(".dropbtn")){var n,o=document.getElementsByClassName("dropdown-content");for(n=0;n<o.length;n++){var s=o[n];s.classList.contains("show")&&s.classList.remove("show")}}};
+//# sourceMappingURL=index.76b1372d.js.map
